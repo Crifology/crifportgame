bdf3a3a9e6c28ce554209de47570c1a01ddb44fd
Crifology <crifology@gmail.com>
2024-11-09 00:18:23 -0500

    removed defaultConfig from vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 // compiles JS in code files to read in browser JS
 // optimizations and compact code - least amount of space
 
-import { defaultConfig, defineConfig } from "vite";
+import { defineConfig } from "vite";
 
 export default defineConfig({
     base: "./",
