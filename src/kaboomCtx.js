825629b3ac469b33fbea5650f9d409dabc53bfbc
Crifology <crifology@gmail.com>
2024-11-09 00:49:02 -0500

    in fact more edits to walls boundaries

--- src/kaboomCtx.js
+++ src/kaboomCtx.js
@@ -1,7 +1,9 @@
 import kaboom from "kaboom";
+import { scaleFactor } from "./constants";
 
 export const k = kaboom({
     global: false,
     touchToMouse: true,     //translate mouse and touch events with mobile
     canvas: document.getElementById("game"),
+    debug: false,
 });
--- src/utils.js
+++ src/utils.js
@@ -32,6 +32,12 @@ export function displayDialogue(text, onDisplayEnd) {
     }
 
     closeBtn.addEventListener("click", onCloseBtnClick);
+
+    addEventListener("keypress", (key) => {
+        if (key.code === "Enter") {
+          closeBtn.click();
+        }
+      });
 }
 
 export function setCamScale(k) {
