15560afac084882496f189072f8b10b605eb79ab
Crifology <crifology@gmail.com>
2024-11-09 00:58:19 -0500

    again

--- src/main.js
+++ src/main.js
@@ -51,24 +51,26 @@ k.scene("main", async () => {
     // Creating Boundaries
     for (const layer of layers) {
         if (layer.name === "boundaries") {
-            for (const boundary of layer.objects) {
-                map.add([
-                    k.area({
-                        shape: new k.Rect(k.vec2(0), boundary.width, boundary.height),
-                    }),
-                    k.body({ isStatic: true }),
-                    k.pos(boundary.x, boundary.y),
-                    boundary.name,
-                ]);
-
-                // collision checks
-                if (boundary.name) {
-                    player.onCollide(boundary.name, () => {
-                        player.isInDialogue = true;
-                        displayDialogue(dialogueData[boundary.name], () => (player.isInDialogue = false))
-                    });
-                }
+          for (const boundary of layer.objects) {
+            map.add([
+              k.area({
+                shape: new k.Rect(k.vec2(0), boundary.width, boundary.height),
+              }),
+              k.body({ isStatic: true }),
+              k.pos(boundary.x, boundary.y),
+              boundary.name,
+            ]);
+    
+            if (boundary.name) {
+              player.onCollide(boundary.name, () => {
+                player.isInDialogue = true;
+                displayDialogue(
+                  dialogueData[boundary.name],
+                  () => (player.isInDialogue = false)
+                );
+              });
             }
+          }
 
             continue;
         }
